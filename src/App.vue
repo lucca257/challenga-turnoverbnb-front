2c53ea6a98d4508d1b610dd135a53cbe362f9148
Pedro Almeida <pedrolucca257@gmail.com.com>
2022-06-01 20:44:13 -0300

    chore: base project

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app>
+
+    <v-app-bar app color="deep-purple accent-4" dark dense>
+      <v-toolbar-title>
+        <v-img
+            alt="Logo"
+            class="shrink mr-2"
+            src="@/assets/principal-icon.png"
+            transition="scale-transition"
+            width="60%"
+        />
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn color="deep-purple accent-4"
+            @click="$router.push({path: item.path})"
+            v-for="item in menuItems"
+            :key="item.icon"
+            :title="item.title"
+            flat
+        >{{ item.title }}</v-btn>
+
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <v-container fluid>
+      <v-slide-y-transition mode="out-in">
+        <v-content>
+          <router-view/>
+        </v-content>
+      </v-slide-y-transition>
+    </v-container>
+
+  </v-app>
+</template>
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      menuItems: [
+        { title: 'Home', path: '/', icon: 'home' },
+        { title: 'About', path: '/about', icon: 'home' },
+      ]
+    }
+  },
+};
+</script>
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>
+              <v-img
+                  alt="Vuetify Logo"
+                  class="shrink mr-2"
+                  src="@/assets/principal-icon.png"
+                  transition="scale-transition"
+                  width="60%"
+              />
+            </v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                  prepend-icon="person"
+                  name="login"
+                  label="Login"
+                  type="text"
+              ></v-text-field>
+              <v-text-field
+                  id="password"
+                  prepend-icon="lock"
+                  name="password"
+                  label="Password"
+                  type="password"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" to="/">Login</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "LoginPage"
+}
+</script>
+
+<style scoped>
+
+</style>
