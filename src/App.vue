053e87055159d3b55e194aa5cb327051d9a45de7
Pedro Almeida <pedrolucca257@gmail.com.com>
2022-06-02 15:42:56 -0300

    refactor: moving code of topBar to a specific component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,6 @@
 <template>
   <v-app>
-
-    <v-app-bar app color="deep-purple accent-4" dark dense>
-      <v-toolbar-title>
-        <v-img
-            alt="Logo"
-            class="shrink mr-2"
-            src="@/assets/principal-icon.png"
-            transition="scale-transition"
-            width="60%"
-        />
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <v-btn color="deep-purple accent-4"
-            @click="$router.push({path: item.path})"
-            v-for="item in menuItems"
-            :key="item.icon"
-            :title="item.title"
-            flat
-        >{{ item.title }}</v-btn>
-
-      </v-toolbar-items>
-    </v-app-bar>
-
+    <topBarComponent></topBarComponent>
     <v-container fluid>
       <v-slide-y-transition mode="out-in">
         <v-content>
@@ -31,20 +8,13 @@
         </v-content>
       </v-slide-y-transition>
     </v-container>
-
   </v-app>
 </template>
 <script>
 export default {
   name: 'App',
-  data(){
-    return {
-      menuItems: [
-        { title: 'Home', path: '/', icon: 'home' },
-        { title: 'Login', path: '/login', icon: 'home' },
-        { title: 'About', path: '/about', icon: 'home' },
-      ]
-    }
-  },
+  components : {
+    'topBarComponent' : () => import('@/components/comum/TopBarComponent.vue'),
+  }
 };
 </script>
---
+++ src/components/comum/TopBarComponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app-bar app color="deep-purple accent-4" dark dense>
+    <v-toolbar-title>
+      <v-img
+          alt="Logo"
+          class="shrink mr-2"
+          src="@/assets/principal-icon.png"
+          transition="scale-transition"
+          width="60%"
+      />
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn
+          color="deep-purple accent-4"
+          @click="$router.push({path: item.path})"
+          v-for="item in menuItems"
+          :key="item.title"
+          :title="item.title"
+          depressed
+      >
+        <v-icon left>
+          {{ item.icon }}
+        </v-icon>
+        {{ item.title }}
+      </v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "TopBarComponent",
+  data(){
+    return {
+      menuItems: [
+        { title: 'Balance', path: '/', icon: 'mdi-scale-unbalanced' },
+        { title: 'Incomes', path: '/', icon: 'mdi-arrow-up-right-bold' },
+        { title: 'Expenses', path: '/', icon: 'mdi-arrow-down-right-bold' },
+        { title: 'Checks', path: '/checks', icon: 'mdi-cash-100' },
+        { title: 'Logout', path: '/logout', icon: 'mdi-logout-variant' },
+      ]
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
