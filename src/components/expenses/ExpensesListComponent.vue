c6076072be81bf3654f25dafd9f4bfec23049a12
Pedro Almeida <pedrolucca257@gmail.com.com>
2022-06-02 22:16:54 -0300

    refactor: uncoupling into components

--- src/components/expenses/ExpensesListComponent.vue
+++ src/components/expenses/ExpensesListComponent.vue
@@ -1,55 +1,42 @@
 <template>
-  <div class="justify-center">
-    <v-card class="mx-auto">
-        <v-card-title>
-          <v-icon>mdi-arrow-down-right-bold</v-icon>
-          Expenses
-        </v-card-title>
-        <v-card-subtitle>June, 2022</v-card-subtitle>
-        <v-simple-table>
-          <template v-slot:default>
-            <thead>
-            <tr>
-              <th class="text-left">
-                Title
-              </th>
-              <th class="text-left">
-                Date
-              </th>
-              <th class="text-left">
-                Hour
-              </th>
-              <th class="text-left">
-                Value
-              </th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr
-                v-for="item in transactions"
-                :key="item.title"
-            >
-              <td>{{ item.title }}</td>
-              <td>{{ item.date }}</td>
-              <td>{{ item.hour }}</td>
-              <td>{{ item.value }}</td>
-            </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
-      </v-card>
-    <v-btn
-        class="mt-3 mx-2 float-right"
-        fab
-        dark
-        color="indigo"
-        title="Add Purchase"
-    >
-      <v-icon dark>
-        mdi-plus
-      </v-icon>
-    </v-btn>
-    </div>
+  <v-card class="mx-auto">
+    <v-card-title>
+      <v-icon>mdi-arrow-down-right-bold</v-icon>
+      Expenses
+    </v-card-title>
+    <v-card-subtitle>June, 2022</v-card-subtitle>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+        <tr>
+          <th class="text-left">
+            Title
+          </th>
+          <th class="text-left">
+            Date
+          </th>
+          <th class="text-left">
+            Hour
+          </th>
+          <th class="text-left">
+            Value
+          </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr
+            v-for="item in transactions"
+            :key="item.title"
+        >
+          <td>{{ item.title }}</td>
+          <td>{{ item.date }}</td>
+          <td>{{ item.hour }}</td>
+          <td>{{ item.value }}</td>
+        </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </v-card>
 </template>
 
 <script>
