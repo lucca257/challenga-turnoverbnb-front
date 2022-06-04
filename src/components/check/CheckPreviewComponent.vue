5081ecf4376055271b23aeb5a96edd370674f663
Pedro Almeida <pedrolucca257@gmail.com.com>
2022-06-03 23:49:51 -0300

    refactor: check preview component

---
+++ src/components/check/CheckPreviewComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h4>Check deposit preview</h4>
+    <v-img :src="imageUrl" style="border: 1px dashed #add8e6; min-height: 250px" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['imageUrl'],
+  name: "CheckPreview"
+}
+</script>
+
+<style scoped>
+
+</style>
