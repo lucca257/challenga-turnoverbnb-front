8202a4587d607ed4fd6bf9ff233402209c29004a
Pedro Almeida <pedrolucca257@gmail.com.com>
2022-06-07 12:46:00 -0300

    feature: display errors in component

--- src/components/login/RegisterComponent.vue
+++ src/components/login/RegisterComponent.vue
@@ -1,54 +1,62 @@
 <template>
-  <v-container fluid fill-height>
-    <v-layout align-center justify-center>
-      <v-flex xs12 sm8 md4>
-        <v-card class="elevation-12">
-          <v-toolbar dark color="primary">
-            <v-toolbar-title>
-              <h2>Register</h2>
-            </v-toolbar-title>
-          </v-toolbar>
-          <v-card-text>
-            <validation-observer ref="obs" v-slot="{passes}" slim>
-              <v-form @submit.prevent="passes(submit)">
-                <validation-provider name="username" rules="required" v-slot="{errors}" slim>
-                  <v-text-field
-                      prepend-icon="person"
-                      v-model="username"
-                      :error-messages="errors[0]"
-                      label="username"
-                      required
-                  />
-                </validation-provider>
-                <validation-provider name="email" rules="required" v-slot="{errors}" slim>
-                  <v-text-field
-                      prepend-icon="mail"
-                      v-model="email"
-                      :error-messages="errors[0]"
-                      label="email"
-                      type="email"
-                      required
-                  />
-                </validation-provider>
-                <validation-provider name="password" rules="required" v-slot="{errors}" slim>
-                  <v-text-field
-                      prepend-icon="lock"
-                      v-model="password"
-                      :error-messages="errors[0]"
-                      label="password"
-                      type="password"
-                      required
-                  />
-                </validation-provider>
-                <v-spacer />&nbsp;&nbsp;
-                <v-btn type="submit" color="info">REGISTER</v-btn>
-              </v-form>
-            </validation-observer>
-          </v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div>
+    <v-alert type="warning"
+             :value="displayError"
+             dismissible>
+      {{displayError}}
+    </v-alert>
+    <v-container fluid fill-height>
+
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+          <v-card class="elevation-12">
+            <v-toolbar dark color="primary">
+              <v-toolbar-title>
+                <h2>Register</h2>
+              </v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+              <validation-observer ref="obs" v-slot="{passes}" slim>
+                <v-form @submit.prevent="passes(submit)">
+                  <validation-provider name="username" rules="required" v-slot="{errors}" slim>
+                    <v-text-field
+                        prepend-icon="person"
+                        v-model="username"
+                        :error-messages="errors[0]"
+                        label="username"
+                        required
+                    />
+                  </validation-provider>
+                  <validation-provider name="email" rules="required" v-slot="{errors}" slim>
+                    <v-text-field
+                        prepend-icon="mail"
+                        v-model="email"
+                        :error-messages="errors[0]"
+                        label="email"
+                        type="email"
+                        required
+                    />
+                  </validation-provider>
+                  <validation-provider name="password" rules="required" v-slot="{errors}" slim>
+                    <v-text-field
+                        prepend-icon="lock"
+                        v-model="password"
+                        :error-messages="errors[0]"
+                        label="password"
+                        type="password"
+                        required
+                    />
+                  </validation-provider>
+                  <v-spacer />&nbsp;&nbsp;
+                  <v-btn type="submit" color="info">REGISTER</v-btn>
+                </v-form>
+              </validation-observer>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -56,8 +64,9 @@ export default {
   name: "RegisterComponent",
   data: () => ({
     username: '',
-    password: '',
-    email: ''
+    password: 'password',
+    email: '@mail.com',
+    displayError: false
   }),
   methods: {
     async submit() {
@@ -74,8 +83,11 @@ export default {
         })
       }).then(async (response) => {
         if (response.status !== 200) {
-          console.log('Looks like there was a problem. Status Code: ' +
-              response.status);
+          const errors = await response.json();
+          for (var prop in errors) {
+            this.displayError = errors[prop]
+            break;
+          }
           return;
         }
         const data = await response.json();
