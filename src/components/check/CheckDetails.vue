585c00e4d4a060f142a53971d22797f82d651f52
Pedro Almeida <pedrolucca257@gmail.com.com>
2022-06-08 00:03:18 -0300

    feature: review check deposit

--- src/components/check/CheckDetails.vue
+++ src/components/check/CheckDetails.vue
@@ -2,7 +2,7 @@
   <v-card class="mx-auto">
     <v-card-title>
       <v-icon>mdi-card-account-details</v-icon>
-      &nbsp; Check details {{ details}}
+      &nbsp; Check details
     </v-card-title>
     <v-card-text>
       <v-container>
@@ -49,10 +49,49 @@
               />
             </validation-provider>
             <checkPreview :imageUrl="customer.imageUrl"/>
-            <v-btn class="mt-4" @click="reject()">
-              <v-icon>mdi-alpha-x-circle</v-icon>
-              &nbsp; REJECT
-            </v-btn> &nbsp;&nbsp;
+            <v-dialog
+                v-model="dialog"
+                persistent
+                max-width="290"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                    class="mt-4"
+                    color="red"
+                    dark
+                    v-bind="attrs"
+                    v-on="on"
+                >
+                  <v-icon>mdi-alpha-x-circle</v-icon>
+                  &nbsp; REJECT
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title class="text-h5">
+                  Are you sure?
+                </v-card-title>
+                <v-card-text>
+                  Are you sure to reject this check?
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                      color="green darken-1"
+                      text
+                      @click="dialog = false"
+                  >
+                    Disagree
+                  </v-btn>
+                  <v-btn
+                      color="red"
+                      text
+                      @click="reject"
+                  >
+                    Agree
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>&nbsp;&nbsp;
             <v-btn type="submit" color="info" class="mt-4">
               <v-icon>mdi-checkbox-marked-circle</v-icon>
               &nbsp; ACCEPT
@@ -81,6 +120,8 @@ export default {
   },
   data () {
     return {
+      dialog: false,
+      accepted: null,
       customer: {
         name: "",
         email: "",
@@ -88,7 +129,6 @@ export default {
         amount: "",
         imageUrl: "",
       },
-      test: null
     }
   },
 
@@ -119,12 +159,40 @@ export default {
         this.displayError = err.message;
       })
     },
-    reject () {
-      this.$parent.checkDetails = false;
+    async reject () {
+      this.accepted = false;
+      await this.requestReview();
     },
-    save () {
-      alert("accept check")
+    async save () {
+      this.accepted = true;
+      await this.requestReview();
     },
+    async requestReview(){
+      await fetch(this.base_url+'admin/deposits/review', {
+        method: 'PATCH',
+        headers: {
+          'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          deposit_id: this.details,
+          accepted: this.accepted,
+        })
+      }).then(async (response) => {
+        if (response.status !== 200) {
+          const errors = await response.json();
+          for (let prop in errors) {
+            this.displayError = errors[prop]
+            break;
+          }
+          return;
+        }
+        this.dialog = false
+        this.$router.go('/')
+      }).catch((err) => {
+        this.displayError = err.message;
+      })
+    }
   },
 }
 </script>
